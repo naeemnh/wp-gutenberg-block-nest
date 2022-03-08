e95be4e9092a551d73c8ec74cce8b6bee8567d64
Naeem Hussain <naeemnh@outlook.com>
2022-03-08 19:20:58 +0400

    adding img and handle functions

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'afdaa4ecf90e50ef4439daf223786613');
+<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '64e842fa235514aaf83208a2cc47c273');
--- src/single-block/edit.js
+++ src/single-block/edit.js
@@ -1,16 +1,65 @@
-import { useBlockProps, RichText } from "@wordpress/block-editor";
+import {
+	useBlockProps,
+	RichText,
+	MediaPlaceholder,
+} from "@wordpress/block-editor";
 import { __ } from "@wordpress/i18n";
+import { Spinner } from "@wordpress/components";
+import { isBlobURL, withNotices } from "@wordpress/blob";
 
-export default function Edit({ attributes, setAttributes }) {
-	const { name, bio } = attributes;
+export default function Edit({
+	attributes,
+	setAttributes,
+	noticeOperations,
+	noticeUI,
+}) {
+	const { name, bio, url, alt } = attributes;
 	const onChangeTitle = (title) => {
 		setAttributes({ title });
 	};
 	const onChangeDesc = (description) => {
 		setAttributes({ description });
 	};
+	const onSelectImage = (image) => {
+		if (!image || !image.url) {
+			setAttributes({ url: undefined, id: undefined, alt: "" });
+			return;
+		}
+		setAttributes({ url: image.url, id: image.id, alt: image.alt });
+	};
+	const onSelectURL = (newURL) => {
+		setAttributes({
+			url: newURL,
+			id: undefined,
+			alt: "",
+		});
+	};
+	const onUploadError = (message) => {
+		noticeOperations.removeAllNotices();
+		noticeOperations.createErrorNotice(message);
+	};
 	return (
 		<div {...useBlockProps()}>
+			{url && (
+				<div
+					className={`wp-block-block-template-single-block-img${
+						isBlobURL(url) ? " is-loading" : ""
+					}`}
+				>
+					<img src={url} alt={alt} />
+					{isBlobURL(url) && <Spinner />}
+				</div>
+			)}
+			<MediaPlaceholder
+				icon="admin-users"
+				onSelect={onSelectImage}
+				onSelectURL={onSelectURL}
+				onError={onUploadError}
+				accept="image/*"
+				allowedTypes={["image"]}
+				disableMediaButtons={url}
+				notices={noticeUI}
+			/>
 			<RichText
 				placeholder={__("Title", "single-block")}
 				tagName="h4"
